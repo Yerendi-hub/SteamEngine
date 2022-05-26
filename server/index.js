ac2b5a80d863d2a601ed4fd8e041e3266d0521ba
Yerendi-hub <konrad.kowalczyk321@gmail.com>
2022-05-26 18:14:19 +0200

    changed port to meet heroku rq

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
     const url = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/' +
