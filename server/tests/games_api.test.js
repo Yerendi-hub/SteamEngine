135b1050c074ce1f8f80b79256ab1b406247bd61
Yerendi-hub <konrad.kowalczyk321@gmail.com>
2022-05-29 20:30:03 +0200

    add tests to ci

--- server/tests/games_api.test.js
+++ server/tests/games_api.test.js
@@ -1,7 +1,18 @@
 const supertest = require('supertest')
 const app = require('../app')
+const dotenv = require('dotenv')
 
 const api = supertest(app)
+
+const OLD_ENV = process.env
+
+afterEach(() => {
+    dotenv.config()
+    jest.clearAllMocks()
+    jest.resetModules()
+    process.env = { ...OLD_ENV }
+    delete process.env.NODE_ENV
+})
 
 test('Gloria Victis game is returned when asking for game 327070', async () => {
     const response = await api.get('/api/games/327070')
