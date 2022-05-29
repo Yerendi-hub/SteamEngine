5c4070cf0dbdf688f254f6d5993113608af336ed
Yerendi-hub <konrad.kowalczyk321@gmail.com>
2022-05-29 12:12:14 +0200

    test and build config

---
+++ server/tests/games_api.test.js
@@ -0,0 +1,16 @@
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('Gloria Victis game is returned when asking for game 327070', async () => {
+    const response = await api.get('/api/games/327070')
+    const json = JSON.parse(response.text)
+    expect(json.game.gameName).toBe('Gloria Victis')
+})
+
+test('Gloria Victis game is returned when asking for game Gloria Victis', async () => {
+    const response = await api.get('/api/games/327070')
+    const json = JSON.parse(response.text)
+    expect(json.game.gameName).toBe('Gloria Victis')
+})
