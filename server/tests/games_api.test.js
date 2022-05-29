caa62fd6635c917db203d87f303c2d2edf7ed8df
Yerendi-hub <konrad.kowalczyk321@gmail.com>
2022-05-29 18:13:52 +0200

    add tests to ci

--- server/tests/games_api.test.js
+++ server/tests/games_api.test.js
@@ -5,12 +5,10 @@ const api = supertest(app)
 
 test('Gloria Victis game is returned when asking for game 327070', async () => {
     const response = await api.get('/api/games/327070')
-    const json = JSON.parse(response.res.text)
-    expect(json.game.gameName).toBe('Gloria Victis')
+    expect(response.body.game.gameName).toBe('Gloria Victis')
 })
 
 test('Gloria Victis game is returned when asking for game Gloria Victis', async () => {
     const response = await api.get('/api/games/327070')
-    const json = JSON.parse(response.res.text)
-    expect(json.game.gameName).toBe('Gloria Victis')
+    expect(response.body.game.gameName).toBe('Gloria Victis')
 })
