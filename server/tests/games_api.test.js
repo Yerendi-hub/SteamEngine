e768e7d727c2c065c0094f6fb7820b6ac0f0808c
Yerendi-hub <konrad.kowalczyk321@gmail.com>
2022-06-02 18:16:41 +0200

    add tests to ci

--- server/tests/games_api.test.js
+++ server/tests/games_api.test.js
@@ -3,6 +3,10 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+
 test('Gloria Victis game is returned when asking for game 327070', async () => {
     const response = await api.get('/api/games/327070')
     expect("Gloria Victis").toBe('Gloria Victis')
