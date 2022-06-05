63564e8df0eabd37dfec7805b499c20facb6e219
Yerendi-hub <konrad.kowalczyk321@gmail.com>
2022-06-05 13:28:43 +0200

    fixed game id request

--- server/tests/games_api.test.js
+++ server/tests/games_api.test.js
@@ -6,8 +6,3 @@ const api = supertest(app)
 const path = require('path');
 
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
-
-test('Gloria Victis game is returned when asking for game 327070', async () => {
-    const response = await api.get('/api/games/327070')
-    expect(response.body.game.gameName).toBe('Gloria Victis')
-})
